56dd621db6e751b8bacf2e85ceba574495b4eadd
bala-altiscale <bala@altiscale.com>
2014-05-29 18:38:49 -0700

    misc

--- scripts/justinstall.sh
+++ scripts/justinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -ex
 ALTISCALE_RELEASE=${ALTISCALE_RELEASE:-0.1.0}
-export RPM_NAME=`echo vcc-tez-${HADOOP_VERSION}-${ARTIFACT_VERSION}`
+export RPM_NAME=`echo vcc-tez-${ARTIFACT_VERSION}`
 echo "Packaging tez rpm with name ${RPM_NAME} with version ${ALTISCALE_VERSION}-${DATE_STRING}"
 
 export RPM_BUILD_DIR=${INSTALL_DIR}/opt
@@ -15,7 +15,7 @@ fpm --verbose \
 --vendor VertiCloud \
 --provides ${RPM_NAME} \
 --description "${DESCRIPTION}" \
---replaces vcc-tez-${HADOOP_VERSION}-${ARTIFACT_VERSION} \
+--replaces vcc-tez-${ARTIFACT_VERSION} \
 -s dir \
 -t rpm \
 -n ${RPM_NAME} \
