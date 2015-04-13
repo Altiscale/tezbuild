18bee332763a17c3142558b05804541aa7da2680
bala-altiscale <bala@altiscale.com>
2015-04-13 14:43:03 -0700

    tar libraries

--- scripts/justinstall.sh
+++ scripts/justinstall.sh
@@ -9,7 +9,7 @@ mkdir --mode=0755 -p ${INSTALL_DIR}/etc/tez
 cd ${RPM_BUILD_DIR}
 export DIST_DIR=${WORKSPACE}/tez/tez-dist/target
 mkdir --mode=0755 lib
-cp -a ${DIST_DIR}/tez-${TEZ_VERSION}/* lib
+tar -xvzf ${DIST_DIR}/tez-${TEZ_VERSION}-minimal.tar.gz -C lib
 cp ${DIST_DIR}/tez-${TEZ_VERSION}.tar.gz .
 cp ${DIST_DIR}/tez-${TEZ_VERSION}-minimal.tar.gz .
 rm -rf ${INSTALL_DIR}/opt/tez
