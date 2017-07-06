57669a8749185d33f31b206855bdba004d24bd8a
Johannes Alberti <johannes@altiscale.com>
2017-07-06 13:59:52 -0700

    Mostly cleanup, and ... - remove /etc/tez to avoid potential conflicts with previous rpms. - remove \n\n from RPM description - always make a clean build, also for the UI - allow running tests and skipping test failures (optionally) - allow for extra options - remove explicit overwrite for the hadoop.version, there are more test failures when build against Hadoop 2.7.2 than when build against Hadoop 2.6.0 (the default)

--- scripts/justbuild.sh
+++ scripts/justbuild.sh
@@ -1,3 +1,20 @@
 #!/bin/sh -ex
 # compile and package the Tez
-mvn package -Dtar -Dhadoop.version=${HADOOP_VERSION} -DskipTests -Dmaven.javadoc.skip=true
+#mvn package -Dtar -Dhadoop.version=${HADOOP_VERSION} -DskipTests -Dmaven.javadoc.skip=true
+#
+env
+MVN_SKIPTESTS_BOOL=${SKIPTESTS_BOOL:-false}
+if [ "${MVN_SKIPTESTS_BOOL}" != "true" ] ; then
+    MVN_SKIPTESTS_BOOL=false
+fi
+
+MVN_IGNORE_TESTFAILURES_BOOL=${IGNORE_TESTFAILURES_BOOL:-false}
+if [ "${MVN_IGNORE_TESTFAILURES_BOOL}" != "true" ] ; then
+    MVN_IGNORE_TESTFAILURES_BOOL=false
+fi
+
+mvn clean -PcleanUICache package -Dtar \
+-Dmaven.test.skip=${MVN_SKIPTESTS_BOOL} -DskipTests=${MVN_SKIPTESTS_BOOL} \
+-Dmaven.test.failure.ignore=${MVN_IGNORE_TESTFAILURES_BOOL} -DtestFailureIgnore=${MVN_IGNORE_TESTFAILURES_BOOL} \
+-Dmaven.javadoc.skip=true \
+${JUSTBUILD_EXTRA_OPTS}
